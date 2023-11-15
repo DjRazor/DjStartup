cb148f1362caf2c70281d3a651003f6082d5d393
Carter Duncan <crduncan1000@gmail.com>
2023-11-14 23:03:47 -0700

    Added express code for server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/user', (req, res) => {
+    
+})
+
+app.use('/users', (_req, res) => {
+    res.sendFile('profile.html', { root: 'public'});
+  });
+
+
+  // Return the application's default page if the path is unknown
+  app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
